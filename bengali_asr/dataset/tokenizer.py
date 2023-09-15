0eca9076e7c3c1348b793891731d4a50b270655e
root <samratthapa120@gmail.com>
2023-09-15 23:35:31 +0900

    whisper tokenizer removed

--- bengali_asr/dataset/tokenizer.py
+++ bengali_asr/dataset/tokenizer.py
@@ -1,5 +1,4 @@
 import torch
-from whisper.tokenizer import get_tokenizer
 
 class CharacterLevelTokenizer:
     def __init__(self,characters : list,start_token=0,end_token=-1) -> None:
@@ -52,32 +51,32 @@ class CharacterLevelCTCTokenizer:
         tokens = tokens.detach().cpu().numpy()
         return "".join([self.idx_to_chars[i] for i in tokens])
 
-class WhisperTokenizer:
+# class WhisperTokenizer:
 
-    def __init__(self,vocabulary) -> None:
-        self.tokenizer = tknizer = get_tokenizer(multilingual=True)
+#     def __init__(self,vocabulary) -> None:
+#         self.tokenizer = tknizer = get_tokenizer(multilingual=True)
 
-        self.chars = {k:self.tokenizer.encode(k) for k in vocabulary}
+#         self.chars = {k:self.tokenizer.encode(k) for k in vocabulary}
 
-        self.start_token = tknizer.sot
-        self.end_token = tknizer.eot
+#         self.start_token = tknizer.sot
+#         self.end_token = tknizer.eot
 
-    def __call__(self,transcript: str,add_extras=True):
-        if add_extras:
-            encoded = [self.start_token]
-        else:
-            encoded=[]
-        for i in transcript:
-            if i in self.chars:
-                encoded.append(self.chars[i])
+#     def __call__(self,transcript: str,add_extras=True):
+#         if add_extras:
+#             encoded = [self.start_token]
+#         else:
+#             encoded=[]
+#         for i in transcript:
+#             if i in self.chars:
+#                 encoded.append(self.chars[i])
 
-        if add_extras:
-            encoded.append(self.end_token)
-        return torch.tensor(encoded)
+#         if add_extras:
+#             encoded.append(self.end_token)
+#         return torch.tensor(encoded)
     
-    def decode(self,tokens):
-        return "".join([self.idx_to_chars[i] for i in tokens])
+#     def decode(self,tokens):
+#         return "".join([self.idx_to_chars[i] for i in tokens])
     
-    def decode_torch_inference(self,tokens):
-        tokens = tokens.detach().cpu().numpy()
-        return "".join([self.idx_to_chars[i] for i in tokens])
+#     def decode_torch_inference(self,tokens):
+#         tokens = tokens.detach().cpu().numpy()
+#         return "".join([self.idx_to_chars[i] for i in tokens])
